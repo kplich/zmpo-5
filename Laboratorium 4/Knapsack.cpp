7b82ef7bed74d4bffd09aef08b4321ad84f2c0f4
Kamil Plich <kamilplich2@gmail.com>
2018-11-29 18:52:21 +0100

    added knapsack class defining a problem instance

---
+++ Laboratorium 4/Knapsack.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "Knapsack.h"
+
+
+Knapsack::Knapsack()
+{
+}
+
+Knapsack::Knapsack(int capacity, int number_of_items, int * item_sizes, int * item_values)
+{
+}
+
+Knapsack::~Knapsack()
+{
+}
