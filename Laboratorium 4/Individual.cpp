0aeb1f1966fe3f566809ef3c46858908b21ca855
Kamil Plich <kamilplich2@gmail.com>
2018-11-30 00:09:36 +0100

    implemented most of individual class

--- Laboratorium 4/Individual.cpp
+++ Laboratorium 4/Individual.cpp
@@ -1,42 +1,117 @@
 #include "pch.h"
 #include "Individual.h"
+#include <random>
+#include <sstream>
 
 
-Individual::Individual()
+Individual::Individual(int size)
 {
+	std::random_device device;
+	std::mt19937 generator(device());
+	std::uniform_int_distribution<> distribution(0, 1);
+
+	genotype = new bool[size];
+
+	for(int i = 0; i < size; i++)
+	{
+		genotype[i] = distribution(generator);
+	}
 }
 
 Individual::Individual(Individual& other)
 {
-	
+	copy_from_another(other);
 }
 
 Individual::~Individual()
 {
+	delete[] genotype;
 }
 
-
-int Individual::evaluate_fitness(KnapsackProblem* problem_instance)
+int Individual::evaluate_fitness(std::vector<std::pair<int, int>*>* items)
 {
-	// TODO: Add your implementation code here.
-	return 0;
+	int result;
+
+	for(int i = 0; i < size; i++)
+	{
+		if(genotype[i])
+		{
+			result += items->at(i)->second;
+		}
+	}
+	return result;
 }
 
 void Individual::mutate(double mutation_probability)
 {
+	std::random_device device;
+	std::mt19937 generator(device());
+
+	for(int i = 0; i < size; i++)
+	{
+		if(std::generate_canonical<double, 10>(generator) < mutation_probability)
+		{
+			genotype[i] = !genotype[i];
+		}
+	}
 }
 
-std::vector<Individual*> Individual::crossover(Individual * other)
+std::pair<Individual*, Individual*>* Individual::crossover(Individual* other, double crossover_probability)
 {
-	return std::vector<Individual*>();
+	std::random_device device;
+	std::mt19937 generator(device());
+
+	Individual* first = new Individual(*this);
+	Individual* second = new Individual(*other);
+
+	if(std::generate_canonical<double, 10>(generator) < crossover_probability)
+	{
+		std::uniform_int_distribution<> distribution(1, size - 1);
+		int cutting_point = distribution(generator);
+
+		//swap genes for each gene, from the cutting point to the end
+		for(int i = cutting_point; i < size; i++)
+		{
+			bool temp = first->genotype[i];
+			first->genotype[i] = second->genotype[i];
+			second->genotype[i] = temp;
+		}
+	}
+	std::pair<Individual*, Individual*>	result(first, second);
 }
 
-Individual & Individual::operator=(const Individual & other)
+std::string Individual::to_string()
 {
-	// TODO: insert return statement here
-	return *(new Individual());
+	std::stringstream sstream;
+
+	sstream << "my genotype:\n";
+
+	for(int i = 0; i < size; i++)
+	{
+		sstream << genotype[i] << "  ";
+	}
+
+	return sstream.str();
 }
 
-void Individual::copy_from_another(const Individual & other)
+Individual& Individual::operator=(const Individual& other)
 {
+	copy_from_another(other);
+	return *this;
+}
+
+void Individual::copy_from_another(const Individual& other)
+{
+	//clean up old data
+	delete[] this->genotype;
+
+	//make room for new data
+	this->size = other.size;
+	this->genotype = new bool[size];
+
+	//copy data
+	for(int i = 0; i < size; i++)
+	{
+		genotype[i] = other.genotype[i];
+	}
 }
--- Laboratorium 4/Individual.h
+++ Laboratorium 4/Individual.h
@@ -3,6 +3,7 @@
 #include "KnapsackProblem.h"
 
 //TODO: idea for later: maybe keep fitness as a variable?
+//TODO: extract random generators
 
 /**
  * \brief Class encoding a possible solution to a problem. Equipped with
@@ -11,22 +12,28 @@
 class Individual
 {
 public:
-	//TODO: probably deletable
-	Individual();
+	//explicit constructor doesn't allow implicit conversions
+	/**
+	 * Constructs an individual of a given size with
+	 * a randomly initialized genotype.
+	 * \param size length of the table encoding the solution (genotype)
+	 */
+	explicit Individual(int size);
+
 	Individual(Individual& other);
+
 	~Individual();
 
 	Individual& operator= (const Individual& other);
 
-
 	/**
 	 * \brief Evaluates the given individual in context of a particular
 	 *		  Knapsack problem
-	 * \param problem_instance instance of the problem on which evaluation
+	 * \param items vector of item sizes and values on which evaluation
 	 *		  of an individual is based
 	 * \return an integer representing fitness of an individual in context of a current problem.
 	 */
-	int evaluate_fitness(KnapsackProblem* problem_instance);
+	int evaluate_fitness(std::vector<std::pair<int, int>*>* items);
 
 	/**
 	 * \brief Mutates every gene of the individual
@@ -39,15 +46,24 @@ public:
 	/**
 	 * \brief Performs a crossover with another individual.
 	 * \param other Pointer to the other individual in the crossover
+	 * \param crossover_probability probability that the individual will swap genes with one another
 	 * \return a vector containing individuals that share their genes with
 	 *		   both crossing-over individuals
 	 */
-	std::vector<Individual*> crossover(Individual* other);
+	std::pair<Individual*, Individual*>* crossover(Individual* other, double crossover_probability);
+
+	std::string to_string();
+
 private:
 	/**
 	 * Table encoding a solution to the problem.
 	 */
 	bool* genotype;
+
+	/**
+	 * Length of the table encoding the solution.
+	 */
+	int size;
 
 	//TODO: fun idea: how about introducing random mutations EVERYTIME an individual is copied?
 	//TODO: implement first, then implement constructors and operators
